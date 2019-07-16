f9be05c40913d134c124721d63dec1b1e4a4df0a
Anas <anas.kaadan@outlook.com>
2019-07-17 01:05:58 +0300

    added C files (north,south,east,west) and separated (facing_up,facing_down) to (facing,up,down)..

--- User/main.c
+++ User/main.c
@@ -45,28 +45,73 @@
 /* Embedded wave variables */
 
 uint32_t *waveAdd[MAX_WAVES] =  {
-	(uint32_t *) &waveByteCode_HiThere[0],
-	(uint32_t *) &waveByteCode_HowAreYou[0],
+	(uint32_t *) &waveByteCode_Forward[0],
+	(uint32_t *) &waveByteCode_Backward[0],
+	(uint32_t *) &waveByteCode_Right[0],
+	(uint32_t *) &waveByteCode_Left[0],
+	(uint32_t *) &waveByteCode_Facing[0],
+	(uint32_t *) &waveByteCode_Up[0],
+	(uint32_t *) &waveByteCode_Down[0],
+	(uint32_t *) &waveByteCode_North[0],
+	(uint32_t *) &waveByteCode_South[0],
+	(uint32_t *) &waveByteCode_East[0],
+	(uint32_t *) &waveByteCode_West[0],
 };
 
 const uint32_t waveLength[MAX_WAVES] =  {
-	WAVEBYTECODE_HITHERE_LENGTH,
-	WAVEBYTECODE_HOWAREYOU_LENGTH,
+	WAVEBYTECODE_FORWARD_LENGTH,
+	WAVEBYTECODE_BACKWARD_LENGTH,
+	WAVEBYTECODE_RIGHT_LENGTH,
+	WAVEBYTECODE_LEFT_LENGTH,
+	WAVEBYTECODE_FACING_LENGTH,
+	WAVEBYTECODE_UP_LENGTH,
+	WAVEBYTECODE_DOWN_LENGTH,
+	WAVEBYTECODE_NORTH_LENGTH,
+	WAVEBYTECODE_SOUTH_LENGTH,
+	WAVEBYTECODE_EAST_LENGTH,
+	WAVEBYTECODE_WEST_LENGTH,
 };
 
 const uint32_t waveResolution[MAX_WAVES] =  {
-	WAVEBYTECODE_HITHERE_BPS,
-	WAVEBYTECODE_HOWAREYOU_BPS,
+	WAVEBYTECODE_FORWARD_BPS,
+	WAVEBYTECODE_BACKWARD_BPS,
+	WAVEBYTECODE_RIGHT_BPS,
+	WAVEBYTECODE_LEFT_BPS,
+	WAVEBYTECODE_FACING_BPS,
+	WAVEBYTECODE_UP_BPS,
+	WAVEBYTECODE_DOWN_BPS,
+	WAVEBYTECODE_NORTH_BPS,
+	WAVEBYTECODE_SOUTH_BPS,
+	WAVEBYTECODE_EAST_BPS,
+	WAVEBYTECODE_WEST_BPS,
 };
 
 const uint32_t waveRate[MAX_WAVES] =  {
-	WAVEBYTECODE_HITHERE_SPS,
-	WAVEBYTECODE_HOWAREYOU_SPS,
+	WAVEBYTECODE_FORWARD_SPS,
+	WAVEBYTECODE_BACKWARD_SPS,
+	WAVEBYTECODE_RIGHT_SPS,
+	WAVEBYTECODE_LEFT_SPS,
+	WAVEBYTECODE_FACING_SPS,
+	WAVEBYTECODE_UP_SPS,
+	WAVEBYTECODE_DOWN_SPS,
+	WAVEBYTECODE_NORTH_SPS,
+	WAVEBYTECODE_SOUTH_SPS,
+	WAVEBYTECODE_EAST_SPS,
+	WAVEBYTECODE_WEST_SPS,
 };
 
 const char *waveName[MAX_WAVES] =  {		// Note wave name must be lower case
-	"hithere",
-	"howareyou",
+	"forward",
+	"backward",
+	"right",
+	"left",
+	"facing",
+	"up",
+	"down",
+	"north",
+	"south",
+	"east",
+	"west",
 };
 
 /* Private function prototypes -----------------------------------------------*/
--- User/wave.h
+++ User/wave.h
@@ -5,39 +5,59 @@
 
 
 /* Maximum number of embedded waves */
-#define MAX_WAVES		6
+#define MAX_WAVES		11
 
 /* Length of embedded waves */
 #define WAVEBYTECODE_FORWARD_LENGTH 				 5795
 #define WAVEBYTECODE_BACKWARD_LENGTH				 6841
 #define WAVEBYTECODE_RIGHT_LENGTH 				   4142
 #define WAVEBYTECODE_LEFT_LENGTH						 2773
-#define WAVEBYTECODE_FACING_UP_LENGTH 			 10143
-#define WAVEBYTECODE_FACING_DOWN_LENGTH			 14625
+#define WAVEBYTECODE_FACING_LENGTH 			     9217
+#define WAVEBYTECODE_UP_LENGTH			         2612
+#define WAVEBYTECODE_DOWN_LENGTH 				     6964
+#define WAVEBYTECODE_NORTH_LENGTH				     5979
+#define WAVEBYTECODE_SOUTH_LENGTH 				   5012
+#define WAVEBYTECODE_EAST_LENGTH						 2579
+#define WAVEBYTECODE_WEST_LENGTH 			       4158
 
 /* Resolution of embedded waves (bits per sample) */
 #define WAVEBYTECODE_FORWARD_BPS 					   8
 #define WAVEBYTECODE_BACKWARD_BPS						 8
 #define WAVEBYTECODE_RIGHT_BPS 					     8
 #define WAVEBYTECODE_LEFT_BPS						     8
-#define WAVEBYTECODE_FACING_UP_BPS 					 8
-#define WAVEBYTECODE_FACING_DOWN_BPS				 8
+#define WAVEBYTECODE_FACING_BPS 					   8
+#define WAVEBYTECODE_UP_BPS				           8
+#define WAVEBYTECODE_DOWN_BPS						     8
+#define WAVEBYTECODE_NORTH_BPS 					     8
+#define WAVEBYTECODE_SOUTH_BPS						   8
+#define WAVEBYTECODE_EAST_BPS 					     8
+#define WAVEBYTECODE_WEST_BPS				         8
 
 /* Rate of embedded waves (sample per second) */
 #define WAVEBYTECODE_FORWARD_SPS 						 16000
 #define WAVEBYTECODE_BACKWARD_SPS						 16000
 #define WAVEBYTECODE_RIGHT_SPS 						   16000
 #define WAVEBYTECODE_LEFT_SPS							   16000
-#define WAVEBYTECODE_FACING_UP_SPS 					 16000
-#define WAVEBYTECODE_FACING_DOWN_SPS				 16000
+#define WAVEBYTECODE_FACING_SPS 					   16000
+#define WAVEBYTECODE_UP_SPS				           16000
+#define WAVEBYTECODE_DOWN_SPS						     16000
+#define WAVEBYTECODE_NORTH_SPS 						   16000
+#define WAVEBYTECODE_SOUTH_SPS							 16000
+#define WAVEBYTECODE_EAST_SPS 					     16000
+#define WAVEBYTECODE_WEST_SPS				         16000
 
 /* Export embedded waves definition */
 extern const uint8_t waveByteCode_Forward[WAVEBYTECODE_FORWARD_LENGTH];
 extern const uint8_t waveByteCode_Backward[WAVEBYTECODE_BACKWARD_LENGTH];
 extern const uint8_t waveByteCode_Right[WAVEBYTECODE_RIGHT_LENGTH];
 extern const uint8_t waveByteCode_Left[WAVEBYTECODE_LEFT_LENGTH];
-extern const uint8_t waveByteCode_Facing_Up[WAVEBYTECODE_FACING_UP_LENGTH];
-extern const uint8_t waveByteCode_Facing_Down[WAVEBYTECODE_FACING_DOWN_LENGTH];
+extern const uint8_t waveByteCode_Facing[WAVEBYTECODE_FACING_LENGTH];
+extern const uint8_t waveByteCode_Up[WAVEBYTECODE_UP_LENGTH];
+extern const uint8_t waveByteCode_Down[WAVEBYTECODE_DOWN_LENGTH];
+extern const uint8_t waveByteCode_North[WAVEBYTECODE_NORTH_LENGTH];
+extern const uint8_t waveByteCode_South[WAVEBYTECODE_SOUTH_LENGTH];
+extern const uint8_t waveByteCode_East[WAVEBYTECODE_EAST_LENGTH];
+extern const uint8_t waveByteCode_West[WAVEBYTECODE_WEST_LENGTH];
 
 /* Export wave variables */
 extern uint32_t *waveAdd[MAX_WAVES];
