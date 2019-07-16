0fccce934e021dce1c63c7770bd56dc2d0f805c2
Anas <anas.kaadan@outlook.com>
2019-07-16 20:58:47 +0300

    added C files (right,left,forward,backward).. modified wave.h file.. deleted old C files (wave_hi_there,wave_how_r_u)..

--- User/wave.h
+++ User/wave.h
@@ -5,23 +5,39 @@
 
 
 /* Maximum number of embedded waves */
-#define MAX_WAVES		2
+#define MAX_WAVES		6
 
 /* Length of embedded waves */
-#define WAVEBYTECODE_HOWAREYOU_LENGTH 				14202
-#define WAVEBYTECODE_HITHERE_LENGTH						9028
+#define WAVEBYTECODE_FORWARD_LENGTH 				 5795
+#define WAVEBYTECODE_BACKWARD_LENGTH				 6841
+#define WAVEBYTECODE_RIGHT_LENGTH 				   4142
+#define WAVEBYTECODE_LEFT_LENGTH						 2773
+#define WAVEBYTECODE_FACING_UP_LENGTH 			 10143
+#define WAVEBYTECODE_FACING_DOWN_LENGTH			 14625
 
 /* Resolution of embedded waves (bits per sample) */
-#define WAVEBYTECODE_HOWAREYOU_BPS 						8
-#define WAVEBYTECODE_HITHERE_BPS							8
+#define WAVEBYTECODE_FORWARD_BPS 					   8
+#define WAVEBYTECODE_BACKWARD_BPS						 8
+#define WAVEBYTECODE_RIGHT_BPS 					     8
+#define WAVEBYTECODE_LEFT_BPS						     8
+#define WAVEBYTECODE_FACING_UP_BPS 					 8
+#define WAVEBYTECODE_FACING_DOWN_BPS				 8
 
 /* Rate of embedded waves (sample per second) */
-#define WAVEBYTECODE_HOWAREYOU_SPS 						22050
-#define WAVEBYTECODE_HITHERE_SPS							22050
+#define WAVEBYTECODE_FORWARD_SPS 						 16000
+#define WAVEBYTECODE_BACKWARD_SPS						 16000
+#define WAVEBYTECODE_RIGHT_SPS 						   16000
+#define WAVEBYTECODE_LEFT_SPS							   16000
+#define WAVEBYTECODE_FACING_UP_SPS 					 16000
+#define WAVEBYTECODE_FACING_DOWN_SPS				 16000
 
 /* Export embedded waves definition */
-extern const uint8_t waveByteCode_HiThere[WAVEBYTECODE_HITHERE_LENGTH];
-extern const uint8_t waveByteCode_HowAreYou[WAVEBYTECODE_HOWAREYOU_LENGTH];
+extern const uint8_t waveByteCode_Forward[WAVEBYTECODE_FORWARD_LENGTH];
+extern const uint8_t waveByteCode_Backward[WAVEBYTECODE_BACKWARD_LENGTH];
+extern const uint8_t waveByteCode_Right[WAVEBYTECODE_RIGHT_LENGTH];
+extern const uint8_t waveByteCode_Left[WAVEBYTECODE_LEFT_LENGTH];
+extern const uint8_t waveByteCode_Facing_Up[WAVEBYTECODE_FACING_UP_LENGTH];
+extern const uint8_t waveByteCode_Facing_Down[WAVEBYTECODE_FACING_DOWN_LENGTH];
 
 /* Export wave variables */
 extern uint32_t *waveAdd[MAX_WAVES];
